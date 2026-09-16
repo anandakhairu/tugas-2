b435f8688dfa10095db8687e7ce6e09a6406d37d
anandakhairu <anandakhairu3108@gmail.com>
2026-09-17 06:40:53 +0700

    Update bagian-12

--- bagian-12.js
+++ bagian-12.js
@@ -1,10 +1,18 @@
+//12.1
 function buildProductLookup(products) {
-  const productMap = new Map();
-  for (const product of products) {
-    productMap.set(product.id, product);
-  }
-  return productMap;
-}
+    const productMap = new Map();
 
+    for (const product of products) {
+        productMap.set(product.id, product);
+    }
+
+    return productMap;
+}
 const productLookup = buildProductLookup(products);
-console.log("Lookup ID 10:", productLookup.get(10));
+
+console.log(productLookup);
+const product = productLookup.get(2);
+
+console.log(product);
+
+console.log(productLookup.has(2));
