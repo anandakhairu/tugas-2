b3bdfb8964f3b27728ba7f5a3f6b98b73d4844cc
anandakhairu <anandakhairu3108@gmail.com>
2026-09-17 06:44:49 +0700

    Update bagian-14
    Added example usage of the Queue class with enqueue and dequeue operations.

--- bagian-14.js
+++ bagian-14.js
@@ -1,14 +1,28 @@
+//14.1 Implementasi Queue
 class Queue {
-  constructor() {
-    this.items = [];
-  }
-  enqueue(item) {
-    this.items.push(item);
-  }
-  dequeue() {
-    return this.items.shift();
-  }
-  peek() {
-    return this.items[0];
-  }
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(item) {
+        this.items.push(item);
+    }
+
+    dequeue() {
+        return this.items.shift();
+    }
+
+    peek() {
+        return this.items[0];
+    }
 }
+const queue = new Queue();
+
+queue.enqueue("Request 1");
+queue.enqueue("Request 2");
+queue.enqueue("Request 3");
+
+console.log(queue.items);
+
+console.log(queue.dequeue());
+console.log(queue.items);
