41d02306165fbe512a306857392e085f50981c49
anandakhairu <anandakhairu3108@gmail.com>
2026-09-17 06:46:11 +0700

    Update bagian-15

--- bagian-15.js
+++ bagian-15.js
@@ -1,8 +1,29 @@
-function printCategories(categories, depth = 0) {
-  for (const category of categories) {
-    console.log(" ".repeat(depth * 2) + category.name);
-    if (category.children && category.children.length > 0) {
-      printCategories(category.children, depth + 1);
+//15.1 Menampilkan Kategori Bertingkat
+const categories = [
+    {
+        name: "Electronics",
+        children: [
+            {
+                name: "Laptop",
+                children: []
+            },
+            {
+                name: "Phone",
+                children: []
+            }
+        ]
     }
-  }
+];
+
+function printCategories(categories, depth = 0) {
+    for (const category of categories) {
+        console.log(" ".repeat(depth) + category.name);
+
+        if (category.children.length > 0) {
+            printCategories(category.children, depth + 1);
+        }
+    }
 }
+
+printCategories(categories);
+
