97878287bcec92ffb3563832b660898233fd0720
anandakhairu <anandakhairu3108@gmail.com>
2026-09-17 06:30:30 +0700

    Refactor linearSearch and add searchProductById function
    Refactor linearSearch function for improved readability and add searchProductById function for product ID search.

--- bagian-6.js
+++ bagian-6.js
@@ -1,15 +1,29 @@
+//6.1
 function linearSearch(array, target) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === target) return i;
-  }
-  return -1;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === target) {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
-function linearSearchById(products, targetId) {
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === targetId) return products[i];
-  }
-  return null;
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(linearSearch(numbers, 30));
+console.log(linearSearch(numbers, 70));
+
+//6.2
+function searchProductById(products, id) {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === id) {
+            return products[i];
+        }
+    }
+
+    return undefined;
 }
 
-console.log("Cari ID 3 (Linear):", linearSearchById(products, 3));
+console.log(searchProductById(products, 2));
+
