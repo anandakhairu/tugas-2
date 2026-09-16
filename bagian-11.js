3a7ab938607051e64e089838049d6526ac5b3ce5
anandakhairu <anandakhairu3108@gmail.com>
2026-09-17 06:39:01 +0700

    Update bagian-11

--- bagian-11.js
+++ bagian-11.js
@@ -1,5 +1,18 @@
-const uniqueCategories = [...new Set(products.map(p => p.category))];
-const uniqueTags = [...new Set(nestedProducts.flatMap(p => p.tags))];
+//11.1 Unique Category
+const uniqueCategories = [
+    ...new Set(products.map(product => product.category))
+];
 
-console.log("Unique Categories:", uniqueCategories);
-console.log("Unique Tags:", uniqueTags);
+console.log(uniqueCategories);
+//11.1 Unique Brand
+const uniqueBrands = [
+    ...new Set(products.map(product => product.brand))
+];
+
+console.log(uniqueBrands);
+//11.1 Unique Tags
+const uniqueTags = [
+    ...new Set(products.flatMap(product => product.tags))
+];
+
+console.log(uniqueTags);
