48d855eedb356d0c6b1b518774ba1319102c6aa6
anandakhairu <anandakhairu3108@gmail.com>
2026-09-17 06:43:13 +0700

    Update bagian-13

--- bagian-13.js
+++ bagian-13.js
@@ -1,27 +1,45 @@
+//13.1 Implementasi Stack
 class Stack {
-  constructor() {
-    this.items = [];
-  }
-  push(item) {
-    this.items.push(item);
-  }
-  pop() {
-    return this.items.pop();
-  }
-  peek() {
-    return this.items[this.items.length - 1];
-  }
-  isEmpty() {
-    return this.items.length === 0;
-  }
+    constructor() {
+        this.items = [];
+    }
+
+    push(item) {
+        this.items.push(item);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
 }
 
+const stack = new Stack();
 
+stack.push("Laptop");
+stack.push("Phone");
+stack.push("Tablet");
+
+console.log(stack.items);
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.items);
+
+//13.2 Search History
 const searchHistory = new Stack();
+
 searchHistory.push("laptop");
 searchHistory.push("phone");
 searchHistory.push("tablet");
 
-console.log("Pencarian Terakhir:", searchHistory.peek()); // tablet
-console.log("Undo Search:", searchHistory.pop()); // tablet dihapus
-console.log("Keyword Sekarang:", searchHistory.peek()); // phone
+console.log(searchHistory.items);
+const previousSearch = searchHistory.pop();
+
+console.log("Kembali ke pencarian:", previousSearch);
